dff10e90c0ccfc62315d2a3591050f5147d3348c
jastuccio <joe@astucc.io>
2019-06-12 10:49:11 -0400

    Add instructions

---
+++ src/helpers.js
@@ -0,0 +1,6 @@
+//This file should export two array helper functions:
+
+/*
+        choice(items): returns a randomly selected item from array of items
+        remove(items, item): removes the first matching item from items, if item exists, and returns it. Otherwise returns undefined.
+*/
---
+++ src/index.js
@@ -0,0 +1,23 @@
+//This file should import the fruits and both array helpers. It should then:
+
+/*
+        Randomly draw a fruit from the array
+        Log the message “I’d like one RANDOMFRUIT, please.”
+        Log the message “Here you go: RANDOMFRUIT”
+        Log the message “Delicious! May I have another?”
+        Remove the fruit from the array of fruits
+        Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
+
+Testing This
+
+Run this with npm start and you should see something like this in your browser console:
+
+I'd like one 🍉, please
+
+Here you go: 🍉
+
+Delicious! May I have another?
+
+I'm sorry, we're all out. We have 14 left.
+
+*/
