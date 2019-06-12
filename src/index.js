6898af4cd06d483fbf65b9c82ade6cc5847ef528
jastuccio <joe@astucc.io>
2019-06-12 16:11:51 -0400

    Add test props

--- src/index.js
+++ src/index.js
@@ -1,28 +1,35 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 class App extends Component {
   render() {
+    const RANDOMFRUIT = "<test> fruit";
+    const FRUITSLEFT = "<test> left";
     return (
       <>
-          <p>I’d like one RANDOMFRUIT, please.<\p>
-          <p>Here you go: RANDOMFRUIT”<\p>
-          <p>Delicious! May I have another?</p>
-          <p>I’m sorry, we’re all out. We have FRUITSLEFT left<\p>
+        <p>I’d like one {RANDOMFRUIT}, please.</p>
+        <p>Here you go: {RANDOMFRUIT}</p>
+        <p>Delicious! May I have another?</p>
+        <p>I’m sorry, we’re all out. We have {FRUITSLEFT} left</p>
       </>
     );
   }
 }
 
-ReactDOM.render(<App />,
-  document.getElementById("root"));
+export default App;
+
+ReactDOM.render(<App />, document.getElementById("root"));
+/*
 
 //This file should import the fruits and both array helpers. It should then:
 
-/*
-        1. Randomly draw a fruit from the array
-        2. Log messages
-        3. Remove the fruit from the array of fruits
-        4. Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
+
+        Randomly draw a fruit from the array
+        Log the message “I’d like one RANDOMFRUIT, please.”
+        Log the message “Here you go: RANDOMFRUIT”
+        Log the message “Delicious! May I have another?”
+        Remove the fruit from the array of fruits
+        Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
 
 Testing This
 
