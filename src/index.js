3d6717c9a314246f4fe4b01161bd3adfd2847684
jastuccio <joe@astucc.io>
2019-06-12 12:33:21 -0400

    Add test messages

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+          <p>I’d like one RANDOMFRUIT, please.<\p>
+          <p>Here you go: RANDOMFRUIT”<\p>
+          <p>Delicious! May I have another?</p>
+          <p>I’m sorry, we’re all out. We have FRUITSLEFT left<\p>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />,
+  document.getElementById("root"));
+
 //This file should import the fruits and both array helpers. It should then:
 
 /*
-        Randomly draw a fruit from the array
-        Log the message “I’d like one RANDOMFRUIT, please.”
-        Log the message “Here you go: RANDOMFRUIT”
-        Log the message “Delicious! May I have another?”
-        Remove the fruit from the array of fruits
-        Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
+        1. Randomly draw a fruit from the array
+        2. Log messages
+        3. Remove the fruit from the array of fruits
+        4. Log the message “I’m sorry, we’re all out. We have FRUITSLEFT left.”
 
 Testing This
 
